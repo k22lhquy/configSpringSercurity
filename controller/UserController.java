bfd058010026e23bf226e6f1a85c1bb1f07b334d
Quy le <quyle150802@gmail.com>
2026-07-29 03:17:25 +0700

    config basic spring boot

--- controller/UserController.java
+++ controller/UserController.java
@@ -1,5 +1,6 @@
 package com.last.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/profile")
-    public String profile(){
-
-        return "User";
+    public String profile(Authentication authentication) {
+    	System.out.println(authentication.getCredentials());
+    	System.out.println(authentication.getDetails());
+    	System.out.println(authentication.getName());
+    	System.out.println(authentication.getPrincipal());
+    	System.out.println(authentication.getAuthorities());
+        return authentication.getName();
 
     }
 
